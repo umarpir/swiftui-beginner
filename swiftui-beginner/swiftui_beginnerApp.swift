1a4dbfeb4959d5b5818eaa090732cd649e2b322e
Umar Pirmahomed <Umar.Pirmahomed@Gamesys.co.uk>
2023-03-06 16:51:41 +0000

    Initial Commit

---
+++ swiftui-beginner/swiftui_beginnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_beginnerApp.swift
+//  swiftui-beginner
+//
+//  Created by Umar Pirmahomed on 06/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_beginnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
