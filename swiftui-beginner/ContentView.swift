34919c33564bdef2390775bdf0d2a80a026db632
Umar Pirmahomed <umarpirmahomed786@gmail.com>
2023-03-16 13:50:14 +0000

    Main: Background Icon and name update

--- swiftui-beginner/ContentView.swift
+++ swiftui-beginner/ContentView.swift
@@ -20,6 +20,8 @@ struct ContentView: View {
                 .multilineTextAlignment(.center)
                 
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.black)
     }
 }
 
